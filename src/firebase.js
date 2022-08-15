798e1dff017a7166a981085406ea3c842ed4ee40
Mwapsam <mwapsam@gmail.com>
2022-08-15 22:41:07 +0200

    User authentication with firebase

---
+++ src/firebase.js
@@ -0,0 +1,60 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  serverTimestamp,
+  doc,
+  orderBy,
+  limit,
+  onSnapshot,
+  query,
+} from 'firebase/firestore';
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  };
+
+initializeApp(firebaseConfig);
+getAnalytics();
+
+const db = getFirestore();
+const auth = getAuth();
+
+export {
+  db,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  serverTimestamp,
+  doc,
+  auth,
+  orderBy,
+  limit,
+  onSnapshot,
+  query,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+};
