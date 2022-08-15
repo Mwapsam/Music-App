1f3ae51700370d85548037e7452080b6355a3331
Mwapsam <mwapsam@gmail.com>
2022-08-15 22:41:48 +0200

    Configure redux store

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Home from './Home';
 import About from './About';
+import Login from './Login';
+import Signup from './Signup';
 
-export { Home, About };
+export { Home, About, Login, Signup };
