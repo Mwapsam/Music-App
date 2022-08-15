8f00912d01d293882b44997840aa66c70e3b2d7b
Mwapsam <mwapsam@gmail.com>
2022-08-15 22:40:23 +0200

    User authentication with firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, About } from "./pages";
+import { Home, About, Login, Signup } from "./pages";
 import { Navbar } from "./components";
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
       <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
     
