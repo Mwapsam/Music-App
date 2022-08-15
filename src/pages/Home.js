f4f83c66fac54ea47cb0ebbb82fa717f8fc300b3
Mwapsam <mwapsam@gmail.com>
2022-08-15 18:36:27 +0200

    Complete style songs grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import songs from '../services/data'
+import styles from '../styles/Home.module.css'
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <div className={styles.song_grid}>
+      {songs.map(song => (
+        <div key={song.id} className={styles.song_card}>
+          <div className={styles.song_content}>
+            <h1 className={styles.song_title}>{song.title}</h1>
+            <h2 className={styles.artist_name}>{song.artist}</h2>
+          </div>
+          <img src={song.image} alt={song.title} className={styles.song_image} />
+        </div>
+      ))}
+    </div>
   )
 }
 
