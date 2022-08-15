2439f94bdc3f1c5a5ee6fb9939528cd4e8b9e513
Mwapsam <mwapsam@gmail.com>
2022-08-15 15:43:30 +0200

    Setup root styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home, About } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <Routes >
+    <div>
+      <Navbar />
+      <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} /> 
-    </Routes>
+      </Routes>
+    </div>
+    
   );
 }
 
