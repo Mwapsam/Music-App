f498827abe048217bca360f6412dae57a1230e70
Mwapsam <mwapsam@gmail.com>
2022-08-15 18:36:46 +0200

    Add dummy data

---
+++ src/services/data.js
@@ -0,0 +1,42 @@
+import { imageZero, imageOne, imageTwo, imageThree, imageFour, imageFive } from "../assets";
+
+const songs = [
+    {
+        id: 1,
+        title: 'Song 1',
+        artist: 'Artist 1',
+        image: imageZero,
+    },
+    {
+        id: 2,
+        title: 'Song 2',
+        artist: 'Artist 2',
+        image: imageOne,
+    },
+    {
+        id: 3,
+        title: 'Song 3',
+        artist: 'Artist 3',
+        image: imageTwo,
+    },
+    {
+        id: 4,
+        title: 'Song 4',
+        artist: 'Artist 4',
+        image: imageThree,
+    },
+    {
+        id: 5,
+        title: 'Song 5',
+        artist: 'Artist 5',
+        image: imageFour,
+    },
+    {
+        id: 6,
+        title: 'Song 6',
+        artist: 'Artist 6',
+        image: imageFive,
+    }
+]
+
+export default songs;
