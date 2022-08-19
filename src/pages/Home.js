4d494aa63c950391a9365c28b3fa554c6adcfb01
Mwapsam <mwapsam@gmail.com>
2022-08-19 21:06:26 +0200

    Create music player component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import songs from '../services/data'
+import { AiFillPlayCircle } from 'react-icons/ai';
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
+  const [searchSong, setSearchSong] = useState('');
+  
+  const songFilter = (e) => {
+    setSearchSong(e.target.value.toLowerCase());
+  }
 
   return (
-    <div className={styles.song_grid}>
-      {songs.map(song => (
-        <div key={song.id} className={styles.song_card}>
+    <>
+      <div className={styles.search_form}>
+        <input type="text" 
+        placeholder="Search the song" 
+        className={styles.search_input} 
+        onChange={songFilter}
+        value={searchSong}
+        />
+      </div>
+      <div className={styles.song_grid}>
+      {songs.filter((song) => {
+        if (searchSong === '') {
+          return song
+        }
+        return song.title.toLowerCase().includes(searchSong)
+      })
+      .map(song => (
+        <Link to={{pathname: `/music/${song.title}`}} key={song.id} className={styles.song_card}>
           <div className={styles.song_content}>
             <h1 className={styles.song_title}>{song.title}</h1>
             <h2 className={styles.artist_name}>{song.artist}</h2>
           </div>
+          <AiFillPlayCircle className={styles.play_icon} />
           <img src={song.image} alt={song.title} className={styles.song_image} />
-        </div>
+        </Link>
       ))}
     </div>
+    </>
   )
 }
 
