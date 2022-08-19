768ac04c1641a3f477a79f7da3360d4cd116062e
Mwapsam <mwapsam@gmail.com>
2022-08-19 21:06:44 +0200

    Create music upload page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Login, Signup } from "./pages";
-import { Navbar } from "./components";
+import { Home, About, Login, Signup, AddMusic } from "./pages";
+import { Navbar, EmailVerification, Player } from "./components";
 
 function App() {
   return (
@@ -12,6 +12,9 @@ function App() {
         <Route path="/about" element={<About />} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/add-music" element={<AddMusic />} />
+        <Route path="/email-verification" element={<EmailVerification />} />
+        <Route path="/music/:name" element={<Player />} />
       </Routes>
     </div>
     
