ec918bb9052da8dc665658d33f00a7a7a7afe197
Mwapsam <mwapsam@gmail.com>
2022-08-19 21:06:14 +0200

    Connect redux store

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import About from './About';
 import Login from './Login';
 import Signup from './Signup';
+import AddMusic from './AddMusic';
 
-export { Home, About, Login, Signup };
+export { Home, About, Login, Signup, AddMusic };
