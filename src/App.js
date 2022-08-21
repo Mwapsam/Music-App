7ece3c57dc196a7a42953272a8fd99a78be2aaa9
Mwapsam <mwapsam@gmail.com>
2022-08-22 02:58:42 +0800

    Add and display music

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Login, Signup, AddMusic } from "./pages";
+import { Home, About, Login, Signup, AddMusic, AdminPlayList } from "./pages";
 import { Navbar, EmailVerification, Player } from "./components";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
         <Route path="/add-music" element={<AddMusic />} />
         <Route path="/email-verification" element={<EmailVerification />} />
         <Route path="/music/:name" element={<Player />} />
+        <Route path="/admin-playlist" element={<AdminPlayList />} />
       </Routes>
     </div>
     
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import About from './About';
 import Login from './Login';
 import Signup from './Signup';
 import AddMusic from './AddMusic';
+import AdminPlayList from './AdminPlayList';
 
-export { Home, About, Login, Signup, AddMusic };
+export { Home, About, Login, Signup, AddMusic, AdminPlayList };
