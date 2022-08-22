cc6304e4659ed60854c705f400d8141a8a5b3601
Mwapsam <mwapsam@gmail.com>
2022-08-23 04:46:45 +0800

    Add CRUD functionalities

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Navbar from "./Navbar";
 import EmailVerification from "./EmailVerification";
 import Player from './Player';
 import CustomPopup from "./CustomPopup";
+import Private from "./Private";
+import Admin from "./Admin";
 
-export { Navbar, EmailVerification, Player, CustomPopup };
+export { Navbar, EmailVerification, Player, CustomPopup, Private, Admin };
