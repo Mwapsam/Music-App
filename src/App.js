0b64e55c6a666d8ca452e5d29de9500bbc06eb28
Mwapsam <mwapsam@gmail.com>
2022-08-26 20:01:55 +0800

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home, About, Login, Signup, AddMusic, AdminPlayList } from "./pages";
-import { Navbar, EmailVerification, Player, Private, Admin } from "./components";
+import { Navbar, EmailVerification, Player, Private } from "./components";
 
 function App() {
   return (
@@ -12,11 +12,11 @@ function App() {
         <Route path="/about" element={<About />} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/add-music" element={(<Admin><AddMusic /></Admin>)} /> */}
+        <Route path="/add-music" element={(<Private><AddMusic /></Private>)} />
         <Route path="/add-music" element={<AddMusic />} />
         <Route path="/email-verification" element={<EmailVerification />} />
         <Route path="/music/:name" element={<Player />} />
-        {/* <Route path="/admin-playlist" element={(<Admin><AdminPlayList /></Admin>)} /> */}
+        <Route path="/admin-playlist" element={(<Private><AdminPlayList /></Private>)} />
         <Route path="/admin-playlist" element={<AdminPlayList />} />
       </Routes>
     </div>
