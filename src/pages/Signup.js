06e992beb2502375a2930185cdba14cf00abaf55
Mwapsam <mwapsam@gmail.com>
2022-08-26 19:50:26 +0800

    Add a loader to signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -11,12 +11,15 @@ import {
   } from '../firebase';
   import { login } from '../redux/auth/userSlice';
   import styles from '../styles/Signup.module.css';
+  import { LoadingSpinner } from '../components';
 
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [displayName, setdisplayName] = useState('');
     const [error, setError] = useState('');
+
+    const [isLoading, setIsLoading] = useState(false);
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -31,6 +34,7 @@ const Signup = () => {
 
     const handleSignup = async (e) => {
         e.preventDefault();
+        setIsLoading(true);
 
         if(!email || !password || !displayName) {
             setError('Please fill all fields');
@@ -41,15 +45,18 @@ const Signup = () => {
             .then( (userAuth) => {
                 updateProfile(auth, displayName)
                     .then( () => {
-                        dispatch(login({
-                            email: userAuth.user.email,
-                            uid: userAuth.user.uid,
-                            displayName: userAuth.user.displayName,
-                            password: userAuth.user.password,
-                            emailVerified: userAuth.user.emailVerified,
+                        setTimeout( async () => {
+                            dispatch(login({
+                                email: userAuth.user.email,
+                                uid: userAuth.user.uid,
+                                displayName: userAuth.user.displayName,
+                                password: userAuth.user.password,
+                                emailVerified: userAuth.user.emailVerified,
 
-                        }
-                        ))
+                            }
+                            ))
+                            setIsLoading(false);
+                        }, 3000)
                         sendEmail();
                     }).catch( (error) => {
                         Error(error);
@@ -57,6 +64,7 @@ const Signup = () => {
                 )
             }).catch( (error) => {
                 setError(error.message);
+                setIsLoading(false);
             }
         )}
 
@@ -77,13 +85,13 @@ const Signup = () => {
   return (
     <div className={styles.main_card}>
         <div className={styles.signup_card}>
-            <form onSubmit={handleSignup} className={styles.signup_form}>
+            {isLoading ? <LoadingSpinner /> : (<form onSubmit={handleSignup} className={styles.signup_form}>
                 <h3 className={styles.form_title}>Register</h3>
                 {error && <p className={styles.errors}>{error}</p>}
                 <input type="text" placeholder="Username" value={displayName} onChange={(e) => setdisplayName(e.target.value)} />
                 <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" className={styles.button}>Register</button>
+                <button type="submit" className={styles.button} disabled={isLoading}>Register</button>
                 <div className={styles.google_btn} onClick={handleGoogleSignup}>
                 <div className={styles.google_icon_wrapper}>
                     <img className={styles.google_icon} src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='google'/>
@@ -91,7 +99,7 @@ const Signup = () => {
                     <p className={styles.btn_text}><b>Signup with google</b></p>
                 </div>
                 <p>Already have an account? <a href="/login">Login</a></p>
-            </form>
+            </form>)}
         </div>
     </div>
   )
